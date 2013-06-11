4c8f450f4afaf5204bd86bac9f8bd78f86f09355
ami1991 <amar.krishnan1991@gmail.com>
2013-06-11 20:49:19 +0530

    Bound Change

--- js/marsRover.js
+++ js/marsRover.js
@@ -7,12 +7,12 @@ A robotic rover is to be landed by NASA on a plateau on Mars.
 
 This plateau, which is curiously rectangular, must be navigated by the rover so that their on-board ameras can get a complete view of the surrounding terrain to send back to Earth.
 
-A rover’s position and location is represented by a combination of x and y co-ordinates and a letter representing one of the four cardinal compass points. The plateau is divided up into a grid to simplify navigation. An example position might be 0, 0, N, which means the rover is in the bottom left corner and facing North.
+A roverï¿½s position and location is represented by a combination of x and y co-ordinates and a letter representing one of the four cardinal compass points. The plateau is divided up into a grid to simplify navigation. An example position might be 0, 0, N, which means the rover is in the bottom left corner and facing North.
 
-In order to control a rover , NASA sends a simple string of letters. The possible letters are ‘L’, ‘R’ and ‘M’. ‘L’ and ‘R’ makes the rover spin 90 degrees left or right respectively, without moving from its current spot. ‘M’ makes the rover move forward one grid position, and maintain the same heading.
+In order to control a rover , NASA sends a simple string of letters. The possible letters are ï¿½Lï¿½, ï¿½Rï¿½ and ï¿½Mï¿½. ï¿½Lï¿½ and ï¿½Rï¿½ makes the rover spin 90 degrees left or right respectively, without moving from its current spot. ï¿½Mï¿½ makes the rover move forward one grid position, and maintain the same heading.
  */
 var maxX=30;
-var maxY=20;
+var maxY=14;
 var gridX=0;
 var gridY=0;
 var x=0;
